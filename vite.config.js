0adf5d6ed48222897611667c5c5eb2edb0303bd8
Artyom Cherdantsev <22115@adata.kz>
2025-06-16 12:09:20 +0500

    refactor

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/daniil-and-alena-wedding/',
   plugins: [vue()],
   resolve: {
     alias: {
